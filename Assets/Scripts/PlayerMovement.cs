3bcbfabe4f9a15c8e21b9d3df45587cecf962406
linusfackler <78768883+linusfackler@users.noreply.github.com>
2022-03-12 18:03:10 -0600

    update

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D body;
+    [SerializeField] private float speed;    
 
-    private load Awake()
+    private void Awake()
     {
-        body = GetComponent<Rigidbody2D>;
+        body = GetComponent<Rigidbody2D>();
     }
+
+    private void Update()
+    {
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+
+        if (Input.GetKey(KeyCode.Space));
+    }
+
 }
