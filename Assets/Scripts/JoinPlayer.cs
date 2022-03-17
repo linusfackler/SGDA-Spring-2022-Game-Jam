905c74bc53efb1f719bc3f521711acbe86f88730
linusfackler <78768883+linusfackler@users.noreply.github.com>
2022-03-16 19:41:29 -0500

    update local multipayer

--- Assets/Scripts/JoinPlayer.cs
+++ Assets/Scripts/JoinPlayer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
+using UnityEngine.InputSystem.Users;
 
 public class JoinPlayer : MonoBehaviour
 {    
