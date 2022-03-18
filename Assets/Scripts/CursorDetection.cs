7efa40041ba8c4f7fb9fb8bc96ae2c0f9b2a16e8
linusfackler <78768883+linusfackler@users.noreply.github.com>
2022-03-17 23:39:47 -0500

    cursor update

--- Assets/Scripts/CursorDetection.cs
+++ Assets/Scripts/CursorDetection.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class CursorDetection : MonoBehaviour
 {
