167c7d7f14ba5b804b89ab38df8d241cf3678286
linusfackler <78768883+linusfackler@users.noreply.github.com>
2022-03-18 05:49:19 -0500

    finished character selection

--- Assets/Scripts/CursorDetection.cs
+++ Assets/Scripts/CursorDetection.cs
@@ -11,40 +11,58 @@ public class CursorDetection : MonoBehaviour
     private PointerEventData pointerEventData = new PointerEventData(null);
 
     public Sprite[] characters;
-    public GameObject[] texts;
+    public Sprite[] fingers;
     public bool chosen = false;
+    public bool done = false;
     public int pickedPlayer;
 
-    private GameObject current, redIMG, blueIMG, blueSquare, redSquare;
+    private GameObject current, redIMG, blueIMG, blueSquare, redSquare, readyRed, readyBlue, startGame;
     private int objectID;
     private Vector3 boxPosition;
+    private int redID, blueID;
 
     void Start()
     {
         gr = GetComponentInParent<GraphicRaycaster>();
-        redIMG = GameObject.Find("RedIMG");
-        blueIMG = GameObject.Find("BlueIMG");
-        redSquare = GameObject.Find("RedSquare");
-        blueSquare = GameObject.Find("BlueSquare");
+        readyBlue = GameObject.Find("ReadyBlue");
+        readyRed = GameObject.Find("ReadyRed");
+        startGame = GameObject.Find("START");
 
+        if (this.gameObject.name == "Player0")
+        {        
+            redIMG = GameObject.Find("RedIMG");
+            redSquare = GameObject.Find("RedSquare");
+            //readyRed.SetActive(false);
+            this.gameObject.GetComponent<Image>().sprite = fingers[0];
+
+
+            // startGame.SetActive(false);
+            // startGame.gameObject.transform.SetAsLastSibling();
+        }
+
+        else if (this.gameObject.name == "Player1")
+        {
+            blueIMG = GameObject.Find("BlueIMG");
+            blueSquare = GameObject.Find("BlueSquare");
+            this.gameObject.GetComponent<Image>().sprite = fingers[1];
+            //readyBlue.SetActive(false);
+        }
+        this.gameObject.GetComponent<Transform>().localScale = new Vector3 (0.2f, 0.2f, 0.2f);
     }
 
     void Update()
     {
-        pointerEventData.position = transform.position;
-        print("pointer position: " + pointerEventData.position);
+        pointerEventData.position = new Vector3 (transform.position.x - 35f, transform.position.y + 10, transform.position.z);
         List<RaycastResult> results = new List<RaycastResult>();
         gr.Raycast(pointerEventData, results);
 
         // if at least 1 result is found, change. Otherwise keep the same
 
-        if (!chosen)
+        if (!chosen && !done)
         {
             if (results.Count > 0)
             {
                 boxPosition = new Vector3(results[0].gameObject.transform.position.x, results[0].gameObject.transform.position.y + 22f, results[0].gameObject.transform.position.z);
-                print(boxPosition);
-                print(results[0].gameObject.name);
                 objectID = int.Parse(results[0].gameObject.name);
                 
                 if (this.gameObject.name == "Player0")
@@ -79,26 +97,72 @@ public class CursorDetection : MonoBehaviour
                 }
             }
         }
-        
     }
 
     public void Select(InputAction.CallbackContext context)
     {
-        if (!chosen)
+        if (context.performed)
         {
-            pickedPlayer = objectID;
-            chosen = true;
+            if (!chosen && !done)
+            {
+                pickedPlayer = objectID;
+                chosen = true;
+
+
+                if (this.gameObject.name == "Player0")
+                {
+                    //readyRed.SetActive(true);
+                    readyRed.transform.SetAsLastSibling();
+                    Debug.Log("Landed here for some reason");
+                }
+
+                else if (this.gameObject.name == "Player1")
+                {
+                    //readyBlue.SetActive(true);
+                    readyBlue.transform.SetAsLastSibling();
+                }
+
+                redID = readyRed.transform.GetSiblingIndex();
+                blueID = readyBlue.transform.GetSiblingIndex();
+                //if (readyRed.activeSelf && readyBlue.activeSelf)
+                if (redID > 6 && blueID > 6)
+                {
+                    startGame.gameObject.transform.SetAsLastSibling();
+                    done = true;
+                }
+            }
         }
+        
     }
 
     public void Back(InputAction.CallbackContext context)
     {
         if (context.started)
         {
-            if (chosen)
+            redID = readyRed.transform.GetSiblingIndex();
+            blueID = readyBlue.transform.GetSiblingIndex();
+            
+            done = false;
+            startGame.gameObject.transform.SetAsFirstSibling();
+
+            chosen = false;
+
+            if (this.gameObject.name == "Player0" && redID > 6)
             {
-                chosen = false;
+                //readyRed.SetActive(false);
+                readyRed.transform.SetAsFirstSibling();
             }
+
+            else if (this.gameObject.name == "Player1" && blueID > 6)
+            {
+                //readyBlue.SetActive(false);
+                readyBlue.transform.SetAsFirstSibling();
+            }
+
+            // else if (this.gameObject.name == "Player0" && redID < 6)
+            // {
+            //     Destroy(gameObject);
+            // }
 
             else
             {
